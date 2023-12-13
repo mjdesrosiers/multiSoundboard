381bc61a79b6ca73b2e33a40f6e1b78141e9856c
mjdesrosiers <mjd2dh@virginia.edu>
2023-12-12 22:17:12 -0500

    Test grabbing events from keyboards

--- main.py
+++ main.py
@@ -36,6 +36,10 @@ sound_map = {
 
 path_noise = "./sounds/noise.mp3"
 
+async def play_noise(channel, noise_path):
+    channel_noise.play(pygame.mixer.Sound(path_noise), -1)
+    channel_noise.set_volume(1.0)
+
 async def print_events(device, channel):
     async for event in device.async_read_loop():
         if event.type == ecodes.EV_KEY:
@@ -48,13 +52,14 @@ async def print_events(device, channel):
 
 pygame.mixer.init()
 channel_noise = pygame.mixer.Channel(0)
-channel_noise.play(pygame.mixer.Sound(path_noise))
-channel_noise.set_volume(0.1)
+
 
 for i, path in enumerate(device_paths):
     dev = evdev.InputDevice(path)
     mixer_channel = pygame.mixer.Channel(i + 1)
     asyncio.ensure_future(print_events(dev, mixer_channel))
+
+asyncio.ensure_future(play_noise(channel_noise, path_noise))
 
 loop = asyncio.get_event_loop()
 loop.run_forever()
